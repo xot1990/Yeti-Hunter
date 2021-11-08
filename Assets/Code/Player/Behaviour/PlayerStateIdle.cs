b104a6479be8db11702c17fc32a966832c733f53
xot1990 <guk.012v@gmail.com>
2021-11-08 14:27:51 +0300

    config

---
+++ Assets/Code/Player/Behaviour/PlayerStateIdle.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateIdle : StateMachine
+{
+    AbstractMob mob;
+    GameObject tongue;
+    float time = 0;
+
+    private void Awake()
+    {
+        OnGetClass<AbstractMob>();
+    }
+
+    private void Start()
+    {
+        tongue = GetComponentInChildren<Tongue>().gameObject;
+    }
+
+    public void OnGetClass<T>() where T : AbstractMob
+    {
+        mob = GetComponent<T>();
+    }
+
+    public override void OnEnterState()
+    {
+        mob.animator.SetBool("OnEat", true);
+        Destroy(GetComponentInChildren<Player_Test>().gameObject);
+    }
+
+    public override void OnUpdateState()
+    {
+        time += Time.deltaTime;
+
+        if (time >= 1)
+        {
+            mob.ChangeState<HuntCM>();
+        }
+    }
+
+    public override void OnExitState()
+    {
+        mob.animator.SetBool("OnEat", false);
+    }
+}
